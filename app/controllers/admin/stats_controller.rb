05c9181c4d069f7c87e376ef44a145a7ee7fec85
kikito <kikito@gmail.com>
2017-01-11 17:24:35 +0100

    Removes ballots from stats controller

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -8,7 +8,6 @@ class Admin::StatsController < Admin::BaseController
     @proposals = Proposal.with_hidden.count
     @spending_proposals = SpendingProposal.with_hidden.count
     @comments = Comment.with_hidden.count
-    @ballot_lines = BallotLine.count
 
     @debate_votes = Vote.where(votable_type: 'Debate').count
     @proposal_votes = Vote.where(votable_type: 'Proposal').count
@@ -24,7 +23,6 @@ class Admin::StatsController < Admin::BaseController
     @user_ids_who_voted_proposals = ActsAsVotable::Vote.where(votable_type: 'Proposal').distinct.count(:voter_id)
     @user_ids_who_didnt_vote_proposals = @verified_users - @user_ids_who_voted_proposals
     @spending_proposals = SpendingProposal.count
-    @ballots_with_votes = Ballot.where("ballot_lines_count > ?", 0).count
   end
 
   def graph
@@ -46,13 +44,6 @@ class Admin::StatsController < Admin::BaseController
   def direct_messages
     @direct_messages = DirectMessage.count
     @users_who_have_sent_message = DirectMessage.select(:sender_id).distinct.count
-  end
-
-  def spending_proposals
-    @ballots = Ballot.where.not(geozone_id: nil).group(:geozone).count
-    @voters_in_city = BallotLine.select(:ballot_id).uniq.joins(:spending_proposal).where("spending_proposals.geozone_id" => nil).to_a.size
-    @voters_in_district = @ballots.values.sum
-    @user_count = Ballot.where('ballot_lines_count > ?', 0).count
   end
 
   def redeemable_codes
